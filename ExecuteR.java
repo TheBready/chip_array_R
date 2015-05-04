b3d90ae0c6a57a56510d7850e7b207faa295caa7
TheBready <Bready@gmx.de>
2015-05-04 21:09:35 +0200

    [Java] Variable für R-Script-Path in runiI()

--- ExecuteR.java
+++ ExecuteR.java
@@ -19,8 +19,8 @@ public class ExecuteR {
 	////////////////////////////////
 	// Ausführen eines R-Skriptes //
 	////////////////////////////////
-	public static void runIt()throws IOException, InterruptedException{
-		Process p = Runtime.getRuntime().exec("cmd runas /profile /user:Administrator /savecred /c start /wait Rscript micro_array_R\\readCel.R");
+	public static void runIt(String rscriptpath)throws IOException, InterruptedException{
+		Process p = Runtime.getRuntime().exec("cmd runas /profile /user:Administrator /savecred /c start /wait Rscript "+rscriptpath);
 		System.out.println("Waiting for R ...");
 	    p.waitFor();
 	    System.out.println("R done.");	
